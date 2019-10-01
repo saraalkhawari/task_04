142820c0ee611596ed75a44c8ce6e6dd9dece25b
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-10-01 19:18:46 +0300

    Task 4 Commit

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+	name = models.CharField(max_length = 105)
+	description = models.TextField()
+	opening_time = models.CharField(max_length = 5) #00:00 
+	closing_time = models.CharField(max_length = 5) #00:00
+
+	def __str__(self):
+		return self.name
